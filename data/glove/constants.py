2b98aaa0553dce7114791be123606330debbf158
Tanmay Gupta <tanmay2099@gmail.com>
2018-08-08 12:40:12 -0500

    add GloveFactory

--- data/glove/constants.py
+++ data/glove/constants.py
@@ -5,10 +5,10 @@ import utils.io as io
 
 class Glove_6B_300d(io.JsonSerializableClass):
     def __init__(
-        self,
-        proc_dir=os.path.join(
-            os.getcwd(),
-            'symlinks/data/glove/proc')):
+            self,
+            proc_dir=os.path.join(
+                os.getcwd(),
+                'symlinks/data/glove/proc')):
         self.proc_dir = proc_dir
         self.embeddings_h5py = os.path.join(
             self.proc_dir,
@@ -18,13 +18,28 @@ class Glove_6B_300d(io.JsonSerializableClass):
             'glove_6B_300d_word_to_idx.json')
         
 
-def glove_contants(tokens='6B',dim='300'):
-    if tokens=='6B':
-        if dim=='300':
-            return Glove_6B_300d()
+class GloveFactory():
+    @staticmethod
+    def create(
+            proc_dir=os.path.join(
+                os.getcwd(),
+                'symlinks/data/glove/proc'),
+            tokens='6B',
+            dim='300'):
+        
+        if tokens=='6B':
+            if dim=='300':
+                glove_cls = Glove_6B_300d
+            else:
+                msg = f'dim {dim} unavailable'
+                assert(False), msg
         else:
-            msg = f'dim {dim} unavailable'
+            msg = f'tokens {tokens} unavailable'
             assert(False), msg
-    else:
-        msg = f'tokens {tokens} unavailable'
-        assert(False), msg
+        
+        return glove_cls(proc_dir)
+
+
+if __name__=='__main__':
+    glove_const = GloveFactory.create()
+    import pdb; pdb.set_trace()
