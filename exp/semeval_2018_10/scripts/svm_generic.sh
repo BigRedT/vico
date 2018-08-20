71b07e257f419753b35a5fa703cb146394f0ed82
Tanmay Gupta <tanmay2099@gmail.com>
2018-08-20 11:16:17 -0500

    add embedding paths to svm_generic.sh

--- exp/semeval_2018_10/scripts/svm_generic.sh
+++ exp/semeval_2018_10/scripts/svm_generic.sh
@@ -1,13 +1,15 @@
 ACTION=$1
 GPU=$2
 
-EXP_NAME='trial'
-OUT_BASE_DIR="${PWD}/symlinks/exp/semeval_2018_10/concat_svm"
+EXP_NAME='glove_rerun3'
+OUT_BASE_DIR="${PWD}/symlinks/exp/semeval_2018_10/concat_svm_random_embeddings"
 
 EMBED_LINEAR_FEAT=False
 EMBED_QUADRATIC_FEAT=True
 DISTANCE_LINEAR_FEAT=True
 DISTANCE_QUADRATIC_FEAT=True
+EMBEDDINGS_H5PY="${PWD}/symlinks/data/glove/proc/glove_6B_300d.h5py"
+WORD_TO_IDX_JSON="${PWD}/symlinks/data/glove/proc/glove_6B_300d_word_to_idx.json"
 
 echo "Running experiment ${EXP_NAME} ..."
 
@@ -21,7 +23,9 @@ then
         --embed_linear_feat $EMBED_LINEAR_FEAT \
         --embed_quadratic_feat $EMBED_QUADRATIC_FEAT \
         --distance_linear_feat $DISTANCE_LINEAR_FEAT \
-        --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT
+        --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT \
+        --embeddings_h5py $EMBEDDINGS_H5PY \
+        --word_to_idx_json $WORD_TO_IDX_JSON
 fi
 
 if [[ "${ACTION}" = *"eval"* ]]
@@ -34,5 +38,7 @@ then
         --embed_linear_feat $EMBED_LINEAR_FEAT \
         --embed_quadratic_feat $EMBED_QUADRATIC_FEAT \
         --distance_linear_feat $DISTANCE_LINEAR_FEAT \
-        --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT
+        --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT \
+        --embeddings_h5py $EMBEDDINGS_H5PY \
+        --word_to_idx_json $WORD_TO_IDX_JSON
 fi
