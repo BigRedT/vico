789be5866bc176cfa9b3263f957711b9c297ab75
Tanmay Gupta <tanmay2099@gmail.com>
2018-09-30 12:17:56 -0500

    add preprocess.sh scripts to semeval and visualgenome

---
+++ data/semeval_2018_10/preprocess.sh
@@ -0,0 +1,4 @@
+python -m data.semeval_2018_10.convert_to_json
+python -m data.semeval_2018_10.compute_word_freq
+python -m data.semeval_2018_10.compute_feature_freq
+python -m data.semeval_2018_10.merge_word_feature_freq
---
+++ data/visualgenome/preprocess.sh
@@ -0,0 +1,3 @@
+#python -m data.visualgenome.create_object_files
+python -m data.visualgenome.compute_object_freqs
+python -m data.visualgenome.compute_attribute_freqs
