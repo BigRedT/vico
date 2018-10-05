d33ee24355b0442d464265f5ae08d9de06d238a3
Tanmay Gupta <tanmay2099@gmail.com>
2018-10-04 20:55:26 -0500

    add merge_object_attribute_freq in visualgenome/preprocess.sh

--- data/visualgenome/preprocess.sh
+++ data/visualgenome/preprocess.sh
@@ -1,3 +1,4 @@
-#python -m data.visualgenome.create_object_files
+python -m data.visualgenome.create_object_files
 python -m data.visualgenome.compute_object_freqs
 python -m data.visualgenome.compute_attribute_freqs
+python -m data.visualgenome.merge_object_attribute_freqs
