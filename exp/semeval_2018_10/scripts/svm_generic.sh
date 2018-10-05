5c1d23700ed4017514cd63adf9b1cd76d67f52c3
Tanmay Gupta <tanmay2099@gmail.com>
2018-10-04 21:03:23 -0500

    update paths in svm_generic.sh

--- exp/semeval_2018_10/scripts/svm_generic.sh
+++ exp/semeval_2018_10/scripts/svm_generic.sh
@@ -2,7 +2,7 @@ ACTION=$1
 GPU=$2
 
 EXP_NAME=$3
-OUT_BASE_DIR="${PWD}/symlinks/exp/semeval_2018_10/normalized_google_images_visual_word_vecs_trained_on_google"
+OUT_BASE_DIR="${PWD}/symlinks/exp/semeval_2018_10/normalized_google_images_recon_loss_visual_word_vecs_trained_on_google"
 
 LR=0.01
 L2_WEIGHT=0.001
@@ -18,8 +18,8 @@ then
     EMBEDDINGS_H5PY="${PWD}/symlinks/data/glove/proc/glove_6B_300d.h5py"
     WORD_TO_IDX_JSON="${PWD}/symlinks/data/glove/proc/glove_6B_300d_word_to_idx.json"
 else
-    EMBEDDINGS_H5PY="${PWD}/symlinks/exp/google_images/normalized_resnet_embeddings_trained_on_google/concat_glove_and_visual/visual_word_vecs.h5py"
-    WORD_TO_IDX_JSON="${PWD}/symlinks/exp/google_images/normalized_resnet_embeddings_trained_on_google/concat_glove_and_visual/visual_word_vecs_idx.json"
+    EMBEDDINGS_H5PY="${PWD}/symlinks/exp/google_images/normalized_resnet_embeddings_recon_loss_trained_on_google/concat_glove_and_visual/visual_word_vecs.h5py"
+    WORD_TO_IDX_JSON="${PWD}/symlinks/exp/google_images/normalized_resnet_embeddings_recon_loss_trained_on_google/concat_glove_and_visual/visual_word_vecs_idx.json"
 fi
 
 echo "Running experiment ${EXP_NAME} ..."
