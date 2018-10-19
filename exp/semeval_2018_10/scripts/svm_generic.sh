b312d7c5a9a3d002478c213e580411662ca1cd84
Tanmay Gupta <tanmay2099@gmail.com>
2018-10-19 11:55:48 -0500

    add GLOVE_DIM in svm_generic.sh

--- exp/semeval_2018_10/scripts/svm_generic.sh
+++ exp/semeval_2018_10/scripts/svm_generic.sh
@@ -7,6 +7,7 @@ OUT_BASE_DIR="${PWD}/symlinks/exp/semeval_2018_10/normalized_google_images_recon
 LR=0.01
 L2_WEIGHT=0.001
 BATCH_SIZE=2560
+GLOVE_DIM=300 #600
 EMBED_LINEAR_FEAT=False
 EMBED_QUADRATIC_FEAT=False
 DISTANCE_LINEAR_FEAT=True
@@ -15,6 +16,7 @@ USE_GLOVE_ONLY=False
 
 if [[ "${USE_GLOVE_ONLY}" = "True" ]]
 then
+    GLOVE_DIM=300
     EMBEDDINGS_H5PY="${PWD}/symlinks/data/glove/proc/glove_6B_300d.h5py"
     WORD_TO_IDX_JSON="${PWD}/symlinks/data/glove/proc/glove_6B_300d_word_to_idx.json"
 else
@@ -37,6 +39,7 @@ then
         --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT \
         --embeddings_h5py $EMBEDDINGS_H5PY \
         --word_to_idx_json $WORD_TO_IDX_JSON \
+        --glove_dim $GLOVE_DIM \
         --batch_size $BATCH_SIZE \
         --lr $LR \
         --l2_weight $L2_WEIGHT
@@ -55,5 +58,6 @@ then
         --distance_quadratic_feat $DISTANCE_QUADRATIC_FEAT \
         --embeddings_h5py $EMBEDDINGS_H5PY \
         --word_to_idx_json $WORD_TO_IDX_JSON \
+        --glove_dim $GLOVE_DIM \
         --batch_size $BATCH_SIZE
 fi
