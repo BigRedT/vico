53425c22ea89865dab18e4206ee276c21b84b0bc
Tanmay Gupta <tanmay2099@gmail.com>
2018-12-13 22:24:58 -0600

    update imagenet run.py

--- exp/imagenet/run.py
+++ exp/imagenet/run.py
@@ -49,7 +49,7 @@ def exp_train():
 
 
 def exp_compute_entity_entity_reps():
-    exp_name = 'multilabel_resnet_50_normalized_adam_entity_entity_reps'
+    exp_name = 'multilabel_resnet_18_fc_center_normalized_adam_entity_entity_reps'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/imagenet')
@@ -60,44 +60,44 @@ def exp_compute_entity_entity_reps():
     data_const = ImagenetDatasetConstants()
 
     model_const = Constants()
-    model_const.model_num = 930000
+    model_const.model_num = 1160000
     model_const.net = ResnetNormalizedConstants()
-    model_const.net.num_layers = 50
+    model_const.net.num_layers = 18
     model_const.net.num_classes = 21841
     model_const.net_path = os.path.join(
         os.getcwd(),
-        'symlinks/exp/imagenet/multilabel_resnet_50_normalized_adam/' + \
+        'symlinks/exp/imagenet/multilabel_resnet_18_fc_center_normalized_adam/' + \
         f'models/net_{model_const.model_num}')
 
     entity_entity_reps.main(exp_const,data_const,model_const)
 
 
 def exp_compute_entity_attr_reps():
-    exp_name = 'resnet_18_normalized_adam_loss_bce_entity_attr_reps'
+    exp_name = 'resnet_18_fc_center_normalized_adam_loss_bce_entity_attr_reps'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/imagenet')
     exp_const = ExpConstants(exp_name,out_base_dir)
-    exp_const.batch_size = 32
+    exp_const.batch_size = 64
     exp_const.num_workers = 5
 
     data_const = ImagenetDatasetConstants()
 
     model_const = Constants()
-    model_const.model_num = 170000
+    model_const.model_num = 210000
     model_const.net = ResnetNormalizedConstants()
     model_const.net.num_layers = 18
     model_const.net.num_classes = 6497
     model_const.net_path = os.path.join(
         os.getcwd(),
-        'symlinks/exp/genome_attributes/resnet_18_normalized_adam_loss_bce/' + \
+        'symlinks/exp/genome_attributes/resnet_18_fc_center_normalized_adam_loss_bce/' + \
         f'models/net_{model_const.model_num}')
 
     entity_attr_reps.main(exp_const,data_const,model_const)
 
 
 def exp_find_nn():
-    exp_name = 'multilabel_resnet_18_normalized_adam_entity_entity_reps'
+    exp_name = 'resnet_18_fc_center_normalized_adam_loss_bce_entity_attr_reps'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/imagenet')
