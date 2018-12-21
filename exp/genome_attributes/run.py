d6afa2a56b92dbf415d541a32493eb517bff8bbb
Tanmay Gupta <tanmay2099@gmail.com>
2018-12-20 18:08:15 -0600

    add runners

--- exp/genome_attributes/run.py
+++ exp/genome_attributes/run.py
@@ -6,13 +6,30 @@ from utils.constants import Constants, ExpConstants
 from .models.resnet_normalized import ResnetNormalizedConstants
 from .dataset import GenomeAttributesDatasetConstants
 from . import train
+from . import train_resnet
 from . import attr_attr_reps
 from . import attr_entity_reps
 from . import find_nn
+from . import class_weights
+from . import attr_cooccur
 
 
-def exp_train():
-    exp_name = 'resnet_18_bn_adam_loss_bce_norm1'
+def exp_compute_class_weights():
+    exp_name = 'class_weights'
+    out_base_dir = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes')
+    exp_const = ExpConstants(exp_name,out_base_dir)
+    exp_const.batch_size = 10
+    exp_const.num_workers = 5
+
+    data_const = GenomeAttributesDatasetConstants()
+
+    class_weights.main(exp_const,data_const)
+
+
+def exp_train_resnet():
+    exp_name = 'resnet_negweighted'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/genome_attributes')
@@ -34,6 +51,48 @@ def exp_train():
     exp_const.margin = 0.5
 
     data_const = GenomeAttributesDatasetConstants()
+    data_const.class_weights_npy = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes/class_weights/class_weights.npy')
+
+    model_const = Constants()
+    model_const.model_num = None
+    model_const.net = ResnetNormalizedConstants()
+    model_const.net.num_layers = 18
+    model_const.net.num_classes = 6497
+    model_const.net_path = os.path.join(
+        exp_const.model_dir,
+        f'net_{model_const.model_num}')
+
+    train_resnet.main(exp_const,data_const,model_const)
+
+
+def exp_train():
+    exp_name = 'resnet_18_mean_adam_loss_balanced_bce_norm1_relu'
+    out_base_dir = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes')
+    exp_const = ExpConstants(exp_name,out_base_dir)
+    exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
+    exp_const.log_dir = os.path.join(exp_const.exp_dir,'log')
+    exp_const.vis_dir = os.path.join(exp_const.exp_dir,'vis')
+    exp_const.log_step = 10
+    exp_const.model_save_step = 10000
+    exp_const.vis_step = 5000
+    # exp_const.val_step = 1000
+    # exp_const.num_val_samples = 1000
+    exp_const.batch_size = 10 # 10 for resnet 18; 5 for resnet 50
+    exp_const.num_epochs = 20
+    exp_const.lr = 0.1
+    exp_const.momentum = 0.9
+    exp_const.num_workers = 5
+    exp_const.optimizer = 'Adam'
+    exp_const.margin = 0.5
+
+    data_const = GenomeAttributesDatasetConstants()
+    data_const.class_weights_npy = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes/class_weights/class_weights.npy')
 
     model_const = Constants()
     model_const.model_num = None
@@ -48,7 +107,7 @@ def exp_train():
 
 
 def exp_compute_attr_attr_reps():
-    exp_name = 'resnet_18_fc_center_normalized_adam_loss_bce_norm1_attr_attr_reps_trial'
+    exp_name = 'resnet_18_mean_adam_loss_balanced_bce_norm1_relu_attr_attr_reps'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/genome_attributes')
@@ -65,7 +124,7 @@ def exp_compute_attr_attr_reps():
     model_const.net.num_classes = 6497
     model_const.net_path = os.path.join(
         os.getcwd(),
-        'symlinks/exp/genome_attributes/resnet_18_fc_center_normalized_adam_loss_bce_norm1/' + \
+        'symlinks/exp/genome_attributes/resnet_18_mean_adam_loss_balanced_bce_norm1_relu/' + \
         f'models/net_{model_const.model_num}')
 
     attr_attr_reps.main(exp_const,data_const,model_const)
@@ -96,7 +155,7 @@ def exp_compute_attr_entity_reps():
 
 
 def exp_find_nn():
-    exp_name = 'multilabel_resnet_18_mean_adam_loss_balanced_bce_norm1_attr_entity_reps'
+    exp_name = 'resnet_18_mean_adam_loss_balanced_bce_norm1_relu_attr_attr_reps'
     out_base_dir = os.path.join(
         os.getcwd(),
         'symlinks/exp/genome_attributes')
@@ -126,29 +185,29 @@ def exp_find_nn():
 
         find_nn.main(exp_const,data_const)
 
-# def exp_eval():
-#     exp_name = 'EXP_NAME'
-#     out_base_dir = os.path.join(
-#         os.getcwd(),
-#         'symlinks/exp/EXP_GROUP')
-#     exp_const = ExpConstants(exp_name,out_base_dir)
-#     exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
-#     exp_const.log_dir = os.path.join(exp_const.exp_dir,'log')
-#     exp_const.vis_dir = os.path.join(exp_const.exp_dir,'vis')
-#     exp_const.batch_size = 32
-#     exp_const.num_workers = 5
 
-#     data_const = DATASET_CONSTANTS()
-#     data_const.subset = 'eval'
+def exp_compute_attr_cooccur():
+    exp_name = 'resnet_18_mean_adam_loss_balanced_bce_norm1_relu_attr_coocur'
+    out_base_dir = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes')
+    exp_const = ExpConstants(exp_name,out_base_dir)
+    exp_const.batch_size = 10
+    exp_const.num_workers = 5
 
-#     model_const = Constants()
-#     model_const.model_num = None
-#     model_const.net = NET_CONSTANTS()
-#     model_const.net_path = os.path.join(
-#         exp_const.model_dir,
-#         f'net_{model_const.model_num}')
+    data_const = GenomeAttributesDatasetConstants()
 
-#     evaluation.main(exp_const,data_const,model_const)
+    model_const = Constants()
+    model_const.model_num = 210000
+    model_const.net = ResnetNormalizedConstants()
+    model_const.net.num_layers = 18
+    model_const.net.num_classes = 6497
+    model_const.net_path = os.path.join(
+        os.getcwd(),
+        'symlinks/exp/genome_attributes/resnet_18_mean_adam_loss_balanced_bce_norm1_relu/' + \
+        f'models/net_{model_const.model_num}')
+
+    attr_cooccur.main(exp_const,data_const,model_const)
 
 
 if __name__=='__main__':
