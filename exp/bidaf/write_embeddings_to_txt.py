e2fce9054106f6cc84025855517a3f5eaea9384c
Tanmay Gupta <tanmay2099@gmail.com>
2018-12-20 18:01:27 -0600

    update paths

--- exp/bidaf/write_embeddings_to_txt.py
+++ exp/bidaf/write_embeddings_to_txt.py
@@ -27,12 +27,12 @@ def write_to_txt(outdir,embeddings_h5py,words_to_idx_json):
 if __name__=='__main__':
     embeddings_dir = os.path.join(
         os.getcwd(),
-        'symlinks/exp/combine_glove_visual_reps/concat_glove_visual_avg_reps')
+        'symlinks/exp/combine_glove_visual_reps/concat_glove_visual_avg_reps_balanced_bce_norm1')
     embeddings_h5py = os.path.join(embeddings_dir,'visual_word_vecs.h5py')
     words_to_idx_json = os.path.join(embeddings_dir,'visual_word_vecs_idx.json')
     outdir = os.path.join(
         os.getcwd(),
-        'symlinks/exp/bidaf/concat_glove_visual_avg_reps')
+        'symlinks/exp/bidaf/concat_glove_visual_avg_reps_balanced_bce_norm1')
     io.mkdir_if_not_exists(outdir,recursive=True)
     write_to_txt(outdir,embeddings_h5py,words_to_idx_json)
     
