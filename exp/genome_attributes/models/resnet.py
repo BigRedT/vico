d569571483ddd8c46a9e7bf3c8bd1cb838ffa9a0
Tanmay Gupta <tanmay2099@gmail.com>
2019-01-15 16:52:31 -0600

    bias in resnet

--- exp/genome_attributes/models/resnet.py
+++ exp/genome_attributes/models/resnet.py
@@ -111,7 +111,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes,bias=True)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
