32cb255e3c888c4421069e5462ecff5ab8d4e8ce
Tanmay Gupta <tanmay2099@gmail.com>
2019-01-15 16:29:45 -0600

    cooccur by types

---
+++ exp/genome_attributes/create_gt_attr_attr_cooccur.py
@@ -0,0 +1,75 @@
+import os
+import copy
+import itertools
+from tqdm import tqdm
+from nltk.corpus import wordnet as wn
+from torch.utils.data import DataLoader
+
+import utils.io as io
+from utils.constants import save_constants
+from .dataset import GenomeAttributesNoImgsDataset
+
+
+def create_gt_synset_cooccur(exp_const,dataloader):
+    print('Creating cooccur ...')
+    cooccur = {}
+    for data in tqdm(dataloader):
+        B = len(data['object_synsets'])
+
+        for b in range(B):
+            for wnid1 in set(data['attribute_synsets'][b]):
+                for wnid2 in set(data['attribute_synsets'][b]):
+                    if wnid1==wnid2:
+                        continue
+
+                    if wnid1 not in cooccur:
+                        cooccur[wnid1] = {}
+                    
+                    if wnid2 not in cooccur[wnid1]:
+                        cooccur[wnid1][wnid2] = 0
+
+                    cooccur[wnid1][wnid2] += 1
+
+                    if wnid2 not in cooccur:
+                        cooccur[wnid2] = {}
+                    
+                    if wnid1 not in cooccur[wnid2]:
+                        cooccur[wnid2][wnid1] = 0
+
+                    cooccur[wnid2][wnid1] += 1
+        
+    synset_cooccur_json = os.path.join(exp_const.exp_dir,'synset_cooccur.json')
+    io.dump_json_object(cooccur,synset_cooccur_json)
+
+    print('Checking symmetry and self constraint in synset cooccur ...')
+    for wnid1, context in tqdm(cooccur.items()):
+        for wnid2, count in context.items():
+            sym_err_msg = f'Word cooccurence not symmetric ({wnid1} / {wnid2})'
+            assert(cooccur[wnid2][wnid1]==count), err_msg
+
+    print('Constraints satisfied')
+    
+
+def main(exp_const,data_const):
+    print(f'Creating directory {exp_const.exp_dir} ...')
+    io.mkdir_if_not_exists(exp_const.exp_dir,recursive=True)
+
+    print('Saving constants ...')
+    save_constants(
+        {'exp': exp_const,'data': data_const},
+        exp_const.exp_dir)
+
+    print('Creating dataloader ...')
+    data_const = copy.deepcopy(data_const)
+    dataset = GenomeAttributesNoImgsDataset(data_const)
+    collate_fn = dataset.create_collate_fn(
+        ['object_synsets','attribute_synsets','attribute_labels_idxs'])
+    dataloader = DataLoader(
+        dataset,
+        batch_size=exp_const.batch_size,
+        shuffle=False,
+        num_workers=exp_const.num_workers,
+        collate_fn=collate_fn)
+
+    create_gt_synset_cooccur(exp_const,dataloader)
+    
---
+++ exp/genome_attributes/create_gt_context_cooccur.py
@@ -0,0 +1,55 @@
+import os
+import copy
+import itertools
+from tqdm import tqdm
+from nltk.corpus import wordnet as wn
+
+import utils.io as io
+from utils.constants import save_constants
+
+
+def create_synset_list(object_annos,obj_ids):
+    synsets = set()
+    for obj_id in obj_ids:
+        anno = object_annos[obj_id]
+        region_synsets = set(anno['object_synsets'])
+        synsets.update(region_synsets)
+
+    return list(synsets)
+
+
+def main(exp_const,data_const):
+    print(f'Creating directory {exp_const.exp_dir} ...')
+    io.mkdir_if_not_exists(exp_const.exp_dir,recursive=True)
+
+    print('Saving constants ...')
+    save_constants(
+        {'exp': exp_const,'data': data_const},
+        exp_const.exp_dir)
+
+    print('Loading data ...')
+    img_id_to_obj_id = io.load_json_object(
+        data_const.image_id_to_object_id_json)
+    object_annos = io.load_json_object(data_const.object_annos_json)
+
+    cooccur = {}
+    for img_id, obj_ids in tqdm(img_id_to_obj_id.items()):
+        synset_list = create_synset_list(object_annos,obj_ids)
+        for synset1 in synset_list:
+            for synset2 in synset_list:
+                if synset1==synset2:
+                    continue
+
+                if synset1 not in cooccur:
+                        cooccur[synset1] = {}
+                    
+                if synset2 not in cooccur[synset1]:
+                    cooccur[synset1][synset2] = 0
+
+                cooccur[synset1][synset2] += 1
+
+    synset_cooccur_json = os.path.join(exp_const.exp_dir,'synset_cooccur.json')
+    io.dump_json_object(cooccur,synset_cooccur_json)
+                
+
+
