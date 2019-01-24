2119b5e0ca7962eb924373773d0d0b7a6b72f67c
Tanmay Gupta <tanmay2099@gmail.com>
2019-01-23 19:16:02 -0600

    2d svm

--- exp/semeval_2018_10/models/concat_svm_simple.py
+++ exp/semeval_2018_10/models/concat_svm_simple.py
@@ -47,6 +47,8 @@ class ConcatSVM(nn.Module,io.WritableToFile):
         self.const = copy.deepcopy(const)
         self.mlp = pytorch_layers.create_mlp(self.const.mlp_const)
         self.bn = nn.BatchNorm1d(self.const.mlp_const['in_dim'])
+        print('2D-SVM')
+        self.w = nn.Parameter(torch.FloatTensor([0,0,0]))
 
     def forward(
             self,
@@ -54,85 +56,62 @@ class ConcatSVM(nn.Module,io.WritableToFile):
             word2_embedding,
             feature_embedding):
         B = word1_embedding.size(0)
-        distance_feats_glove = self.compute_distance_features(
+        score_glove = self.compute_distance_features_glove(
             word1_embedding[:,:self.const.glove_dim],
             word2_embedding[:,:self.const.glove_dim],
             feature_embedding[:,:self.const.glove_dim])
         if self.const.embedding_dim > self.const.glove_dim:
-            distance_feats_visual = self.compute_distance_features(
+            score_visual = self.compute_distance_features_visual(
                 word1_embedding[:,self.const.glove_dim:],
                 word2_embedding[:,self.const.glove_dim:],
                 feature_embedding[:,self.const.glove_dim:])
             if self.const.visual_only==True:
-                distance_feats = torch.cat((
-                    0*distance_feats_glove,
-                    distance_feats_visual),1)
+                score = self.w[1]*score_visual + self.w[2]
             else:
-                distance_feats = torch.cat((
-                    distance_feats_glove,
-                    distance_feats_visual),1)
+                score = \
+                    self.w[0]*score_glove + \
+                    self.w[1]*score_visual + \
+                    self.w[2]       
         else:
-            distance_feats = distance_feats_glove
-            
-        x = self.bn(distance_feats)
-        score = self.mlp(x)[:,0] # Convert Bx1 to B
+            score = self.w[0]*score_glove + self.w[2]
+
         return score
 
-    def compute_distance_features(self,word1,word2,feature):
+    def compute_distance_features_glove(self,word1,word2,feature):
         B = word1.size(0)
         cs_word1_feat = self.cosine_similarity(word1,feature)
         cs_word2_feat = self.cosine_similarity(word2,feature)
         cs_word1_word2 = self.cosine_similarity(word1,word2)
-        l1_word1_feat = self.l1_norm(word1-feature)
-        l1_word2_feat = self.l1_norm(word2-feature)
-        l1_word1_word2 = self.l1_norm(word1-word2)
-        distance_feats = torch.cat((
-            cs_word1_feat,
-            cs_word2_feat,
-            cs_word1_word2,
-            l1_word1_feat/(self.const.embedding_dim),
-            l1_word2_feat/(self.const.embedding_dim),
-            l1_word1_word2/(self.const.embedding_dim)),1)
-        distance_quadratic_feats = \
-            torch.unsqueeze(distance_feats,1) * \
-            torch.unsqueeze(distance_feats,2)
-        distance_quadratic_feats = distance_quadratic_feats.view(B,-1)
-        x = torch.cat((
-            self.const.use_distance_linear_feats*distance_feats,
-            self.const.use_distance_quadratic_feats*distance_quadratic_feats),1)
+        x = cs_word1_feat - cs_word2_feat
         return x
+
+    def compute_distance_features_visual(self,word1,word2,feature):
+        B = word1.size(0)
+        cs_word1_feat = self.cosine_similarity(word1,feature)
+        cs_word2_feat = self.cosine_similarity(word2,feature)
+        cs_word1_word2 = self.cosine_similarity(word1,word2)
+        x = cs_word1_feat - cs_word2_feat
+        return x
+
 
     def compute_hinge_loss(self,score,label):
         scaled_label = 2*label-1
-        return torch.mean(torch.max(0*score,1-scaled_label*score))
+        return torch.mean(torch.max(0*score,0.2-scaled_label*score[:,0]))
 
     def compute_l2_loss(self):
-        # l2_reg = None
-        # for name, W in self.named_parameters():
-        #     if 'bias' in name:
-        #         continue
-        #     if 'embedding' in name:
-        #         continue
-        #     if l2_reg is None:
-        #         l2_reg = W.pow(2).sum()
-        #     else:
-        #         l2_reg = l2_reg + W.pow(2).sum()
-        #     import pdb; pdb.set_trace()
-        # return l2_reg
-        W = self.mlp.layers[0][0].weight
-        l2_reg = W.pow(2).sum()
+        l2_reg = self.w.pow(2).sum()
         return l2_reg
 
     def compute_loss(self,score,label):
         hinge_loss = self.compute_hinge_loss(score,label)
-        l2_reg = self.compute_l2_loss()
-        total_loss = hinge_loss + (self.const.l2_weight * l2_reg)
+        l2_reg = self.const.l2_weight*self.compute_l2_loss()
+        total_loss = hinge_loss + l2_reg
         return total_loss, hinge_loss, l2_reg
 
     def cosine_similarity(self,a,b):
         a_norm = torch.norm(a,2,1,keepdim=True)
         b_norm = torch.norm(b,2,1,keepdim=True)
-        return torch.sum(a*b,1,keepdim=True)#/(a_norm*b_norm+1e-6)
+        return torch.sum(a*b,1,keepdim=True)/(a_norm*b_norm+1e-6)
 
     def l1_norm(self,x):
         return torch.sum(torch.abs(x),1,keepdim=True)
