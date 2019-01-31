b15ac4b5c5bc880044ee9ada54dadcf9d9730d9e
Tanmay Gupta <tanmay2099@gmail.com>
2019-01-31 14:57:13 -0600

    update vsepp paths

--- exp/vsepp/write_vocab_embeddings.sh
+++ exp/vsepp/write_vocab_embeddings.sh
@@ -5,13 +5,13 @@ VOCAB_PKL="/home/nfs/tgupta6/Code/vsepp/vocab/coco_precomp_vocab.pkl"
 # VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/glove_6B_300d.h5py"
 # VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/glove_6B_300d_word_to_idx.json"
 
-VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/no_syn_cooccur_self_count_dim_100_neg_no_decay/concat_with_glove_300"
+VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_attr_extract/no_syn_extract_cooccur_self_count_dim_100_neg_no_decay_two_embed_kl/concat_with_glove_300"
 VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/visual_word_vecs.h5py"
 VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/visual_word_vecs_idx.json"
 VISUAL_WORDS_JSON="${VISUAL_WORD_VECS_DIR}/visual_words.json"
 
 
-VSEPP_WORD_VECS_NPY="/home/nfs/tgupta6/Code/vsepp_data/data/coco_precomp/no_syn_cooccur_self_count_dim_100_neg_no_decay_concat_with_glove_300.npy"
+VSEPP_WORD_VECS_NPY="/home/nfs/tgupta6/Code/vsepp_data/data/coco_precomp/no_syn_extract_cooccur_self_count_dim_100_neg_no_decay_two_embed_kl_concat_with_glove_300.npy"
 
 # Comment out visual_words_json when using glove
 python -m exp.vsepp.write_vocab_embeddings \
