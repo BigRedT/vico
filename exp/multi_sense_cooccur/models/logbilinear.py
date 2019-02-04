90d2ce9273d26479b1ee5711e1eb10ab2accc24d
Tanmay Gupta <tanmay2099@gmail.com>
2019-02-04 12:09:47 -0600

    multiple xforms

--- exp/multi_sense_cooccur/models/logbilinear.py
+++ exp/multi_sense_cooccur/models/logbilinear.py
@@ -15,24 +15,65 @@ class TransformConstants(io.JsonSerializableClass):
         self.in_feat = 100
         self.out_feat = 50
         self.identity = False
+        self.xform_type = 'linear' # identity | linear | affine | nonlinear | select
+        # begin and end only used with select
+        self.begin = 0
+        self.end = 99
+        # num_layers only used with nonlinear
+        self.num_layers = None
 
 
 class Transform(nn.Module,io.WritableToFile):
     def __init__(self,const):
         super(Transform,self).__init__()
         self.const = copy.deepcopy(const)
-        self.fc1 = nn.Linear(self.const.in_feat,self.const.out_feat,bias=False)
-        # self.relu = nn.ReLU()
-        # self.fc2 = nn.Linear(self.const.out_feat,self.const.out_feat)
+        self.layers = self.construct_layers()
+
+    def construct_layers(self):
+        if self.const.xform_type=='linear':
+            return nn.Sequential(
+                nn.Linear(self.const.in_feat,self.const.out_feat,bias=False))
+        elif self.const.xform_type=='affine':
+            return nn.Sequential(
+                nn.Linear(self.const.in_feat,self.const.out_feat))
+        elif self.const.xform_type=='select':
+            return None
+        elif self.const.xform_type=='identity':
+            return None
+        elif self.const.xform_type=='nonlinear':
+            if self.const.num_layers==2:
+                out_feat_ = (self.const.in_feat + self.const.out_feat)//2
+                return nn.Sequential(
+                    nn.Linear(self.const.in_feat,out_feat_),
+                    nn.Tanh(), #nn.ReLU(),
+                    nn.Linear(out_feat_,self.const.out_feat))
+            elif self.const.num_layers==4:
+                d = (self.const.out_feat - self.const.in_feat)//4
+                in_feat = self.const.in_feat
+                return nn.Sequential(
+                    nn.Linear(in_feat,in_feat+d),
+                    nn.Tanh(), #nn.ReLU(),
+                    nn.Linear(in_feat+d,in_feat+2*d),
+                    nn.Tanh(), #nn.ReLU(),
+                    nn.Linear(in_feat+2*d,in_feat+3*d),
+                    nn.Tanh(), #nn.ReLU(),
+                    nn.Linear(in_feat+3*d,self.const.out_feat))
+            else:
+                err_msg = f'Not implemented num_layers {self.const.num_layers}'
+                assert(False), err_msg
+        else:
+            err_msg = f'Not implemented num_layers {self.const.xform_type}'
+            assert(False), err_msg
+                
 
     def forward(self,x):
-        if self.const.identity==True:
-            return x
+        if self.const.xform_type=='select':
+            return x[:,self.const.begin:self.const.end+1]
 
-        x = self.fc1(x)
-        # x = self.relu(x)
-        # x = self.fc2(x)
-        return x
+        if self.const.xform_type=='identity':
+            return x
+        
+        return self.layers(x)
 
 
 class LogBilinearConstants(io.JsonSerializableClass):
@@ -42,22 +83,43 @@ class LogBilinearConstants(io.JsonSerializableClass):
         self.embed_dims = 100
         self.two_embedding_layers = False
         self.cooccur_types = [
-            'syn',
+            #'syn',
             'attr_attr',
             'obj_attr',
             'obj_hyp',
             'context'
         ]
-        self.xform_out_feats = [50]*5
+        self.xform_out_feats = [50]*4
+        self.xform_type = 'linear'
+        # xform_num_layers used on if type is 'nonlinear
+        self.xform_num_layers = 2
+        self.use_bias = False
         
     @property
     def xform_const(self):
         const = {}
         for i,cooccur_type in enumerate(self.cooccur_types):
             const_ = TransformConstants()
-            const_.in_feat = self.embed_dims
-            const_.out_feat = self.xform_out_feats[i]
+            
+            if self.xform_type=='select':
+                const_.in_feat = self.embed_dims // len(self.cooccur_types)
+                const_.begin = i*const_.in_feat
+                const_.end = (i+1)*const_.in_feat - 1
+            else:
+                const_.in_feat = self.embed_dims
+                const_.begin = None
+                const_.end = None
+            
+            if self.xform_type=='identity':
+                const_.out_feat = const_.in_feat
+            else:
+                const_.out_feat = self.xform_out_feats[i]
+                
+            const_.xform_type = self.xform_type
+            const_.num_layers = self.xform_num_layers
+
             const[cooccur_type] = const_
+
         return const
 
     @property
@@ -96,7 +158,9 @@ class LogBilinear(nn.Module,io.WritableToFile):
         w1 = xform(w1)
         w2 = xform(w2)
         i = self.const.cooccur_type_to_idx[cooccur_type]
-        scores = torch.sum(w1*w2,1) + b1[:,i] + b2[:,i]
+        scores = torch.sum(w1*w2,1)
+        if self.const.use_bias==True:
+            scores = scores + b1[:,i] + b2[:,i]
         return scores
 
     def loss(self,scores,target,x):
