d6bdb1036225061d3cf688ddce3b2af355c4a01b
Tanmay Gupta <tanmay2099@gmail.com>
2019-02-11 11:07:11 -0600

    add categories

---
+++ exp/multi_sense_cooccur/vis/fine_categories.py
@@ -0,0 +1,176 @@
+# Food
+FOOD_MISC = {'dough','cheese','food','egg','wheat','rice','butter','oil'}
+
+FOOD_SPICES = {'salt','pepper','spice','chili','garlic','ginger'}
+
+FOOD_HERB = {'oregano','herb','parsley','basil'}
+
+FOOD_MEALS = {'lunch','meal','breakfast','dinner'}
+
+FOOD_PREP = {'dish','chip','tortilla','burger','toast','bagel','pizza',
+    'pasta','sauce','salad','bread','pickle','bun','soup','noodle','syrup',}
+
+FOOD_DESSERT = {'muffin','cake','pancake','sweet','candy','dessert','cupcake',
+    'doughnut','pastry','sugar',}
+
+FOOD_FRUITS = {'apple','banana','fruit','pineapple','mango','pear','berry',
+    'lime','lemon','peach','plum','date'}
+
+FOOD_VEG = {'tomato','zucchini','broccoli','vegetable','capsicum','spinach',
+    'onion','pea','squash','potato','corn','bean','cabbage','mushroom',
+    'carrot'}
+
+FOOD_NUTS = {'nut','almond','cashew','pecan','peanut','walnut','hazelnut'}
+
+FOOD_DRINKS = {'coffee','tea','water','juice','beer','wine','alcohol','drink',
+    'milk'}
+
+FOOD_MEAT = {'pepperoni','steak','chicken','meat','pork'}
+
+# Animals
+ANIMALS_WILD = {'zebra','elephant','giraffe','lion','tiger','monkey','antelope',
+    'bear','animal','gazelle'}
+
+ANIMALS_PETS = {'dog','cat','kitten','puppy'}
+
+ANIMALS_FARM = {'ox','cow','goat','cattle','bull','lamb','horse','donkey'}
+
+ANIMALS_BIRDS = {'bird','turkey','owl','sparrow','pigeon','ostrich','duck',
+    'goose','swan','gull','flamingo','peacock'}
+
+ANIMALS_REPTILES = {'snake','reptile','turtle','crocodile','lizard'}
+
+ANIMALS_FISH = {'fish','dolphin','shark'}
+
+# Colors
+COLORS = {'red','green','blue','yellow','brown','grey','black','white',
+    'orange','purple','pink','cyan','violet','indigo','gold','silver',
+    'maroon'}
+
+# Applicance
+APPLIANCES = {'refrigerator','toaster','oven','burner','dishwasher',
+    'blender','microwave','oven','stove','appliance','cooler'}
+
+# Electronics
+ELECTRONICS_COMP = {'computer','laptop','keyboard','mouse','mousepad',
+    'printer'}
+
+ELECTRONICS_DISPLAY = {'monitor','television','tv','display'}
+
+ELECTRONICS_AUDIO = {'earbud','headphone','speaker','microphone'}
+
+ELECTRONICS_COMM = {'cellphone','phone','antenna','radio','telephone'}
+
+ELECTRONICS_MISC = {'electonic','device','digital','clock','camera',
+    'electronics'}
+
+# Utensils
+UTENSILS_CONTAINERS = {'cup','bowl','utensil','plate','jar','vase','urn'}
+
+UTENSILS_DRINKS = {'glass','bottle','jug','fork'}
+
+UTENSILS_CUTLERY = {'spoon','spatula',}
+
+UTENSILS_COOKING = {'pan','kettle','teapot','pot'}
+
+# Transport
+TRANSPORT_FOURWHEEL = {'car','bus','minivan','tractor','buggy','van',
+    'minivan','jeep'}
+
+TRANSPORT_TWOWHEEL = {'scooter','bike','bicycle','motorcycle','moped'}
+
+TRANSPORT_RAIL = {'train','tram','railway','engine'}
+
+TRANSPORT_WATER = {'boat','ship','kayak'}
+
+TRANSPORT_AIR = {'aircraft','helicopter','jet','aeroplane','propeller'}
+
+TRANSPORT_GENERIC = {'vehicle','transport','cargo'}
+
+# Humans
+HUMANS_PROFESSION = {'worker','firefighter','fireman','doctor','soldier',
+    'photographer','accountant','refree','student'}
+
+HUMANS_MALE = {'man','male','boy','father'}
+
+HUMANS_FEMALE = {'female','woman','girl','miss','mother','lady','girl'}
+
+HUMANS_NEUTRAL = {'people','person','individual','friend','lodger'}
+
+HUMANS_COLOR = {'caucasian','brunet','blonde'}
+
+HUMANS_SPORT = {'skier','snowboarder''skateboarder'}
+
+HUMANS_AGE = {'young','old','adolescent','teen','teenager','adult','child',
+    'baby'}
+
+HUMANS_COMMUTE = {'motorcyclist','cyclist','driver','bicyclist','motorist',
+    'pedestrian','rider'}
+
+# Numbers
+NUMBERS = {'numeral','number','one','two','three','four','five','six',
+    'seven','eight','nine','ten'}
+
+# Clothes
+CLOTHES_COATS = {'robe','blazer','jacket','coat'}
+
+CLOTHES_FULL_BODY = {'dress','apparel','suit','outfit','clothing','uniform',
+    'overall','full-dress'}
+
+CLOTHES_SCANT = {'swimsuit','bikini','lingerie','negligee'}
+
+CLOTHES_LEGS = {'boot','sandal','shoe','slipper','legging','trouser','skirt',
+    'sock','jean','hosiery','stocking','pajama'}
+
+CLOTHES_ARMS = {'wristband','mitten','glove','sleeve','watch'}
+
+CLOTHES_HEAD = {'sunglasses','spectacle','visor','helmet','beanie','cap','hat',
+    'headband','bandanna','hood'}
+
+CLOTHES_NECK = {'bib','tie','scarf','necktie'}
+
+CLOTHES_TORSO = {'shirt','tshirt','t-shirt','top','blouse','apron','jersey',
+    'sweatshirt','sweater','lapel'}
+
+# Buildings
+CONSTRUCTION_BUILDINGS = {'hotel','hostel','lodge','building','church','barn',
+    'shed','restaurant','church','hut'}
+
+CONSTRUCTION_INDOORS = {'bedroom','room','stairway','hallway','patio','kitchen',
+    'bathroom','railing','balcony','ledge','lounge'}
+
+CONSTRUCTION_OUTDOORS = {'ledge','balcony','roof','rooftop'}
+
+CONSTRUCTION_COMMERCIAL = {'booth','stall','market','plaza','shop','shopfront',
+    'mall'}
+
+CONSTRUCTION_FIXTURES = {'door','window','knob','faucet','lightbulb','bulb','latch',
+    'chandelier','fixture','sink','fireplace','bathtub'}
+
+CONSTRUCTION_FURNITURE = {'chair','table','countertop','counter','cabinet','desk','bed',
+    'bench','cupboard','furniture','bookcase','armchair','sofa','bench','pew',
+    'stool','armchair','bookcase','seat','couch'}
+
+# Actions
+ACTIONS = {'call','take','step','come','get','bit','rid','throw','catch',
+    'enjoy','smile','eat','look','walk','stand','kneel','crouch','bend',
+    'talk','leave','construct','make','hit','keep','play','wait','relax',
+    'sit','read','serve','fix','lean','leave','kick','squat','bow','swing',
+    'get','go','gravel','annoy','rest','put','sleep','catch'}
+
+# Bodyparts
+BODYPARTS_FACE = {'ear','nose','nostril','head','lip','cheek','face','tounge',
+    'tooth','chin','thumb','elbow',}
+
+BODYPARTS_HAIR = {'hair','feather','tuft','eyebrow','brow','mane','eyelash','fur',
+    'ponytail','beard'}
+
+BODYPARTS_LIMBS = {'leg','arm','foot','thigh','calf','limb'}
+
+BODYPARTS_JOINTS = {'knee','wrist','ankle','shoulder'}
+
+BODYPARTS_TORSO = {'thorax','waist','stomach','belly','abdomen','neck','hip'}
+
+BODYPARTS_EXTREMITIES = {'fingernail','finger','toe','toenail','hand'}
+
+BODYPARTS_NON_HUMAN = {'tail','horn','claw','hoof'}
