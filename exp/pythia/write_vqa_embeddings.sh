fdd8af398c9d9a86b8feee3ccd2d851f78981562
Tanmay Gupta <tanmay2099@gmail.com>
2019-02-18 21:40:38 -0600

    update paths

--- exp/pythia/write_vqa_embeddings.sh
+++ exp/pythia/write_vqa_embeddings.sh
@@ -3,7 +3,9 @@
 # VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/glove_6B_300d.h5py"
 # VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/glove_6B_300d_word_to_idx.json"
 
-VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/no_syn_cooccur_self_count_dim_100_neg_no_decay/xformed_concat_with_glove_300"
+VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/\
+exp/multi_sense_cooccur/imagenet_genome_gt/effect_of_xforms/\
+dim_200_neg_bias_select/concat_with_glove_300"
 VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/visual_word_vecs.h5py"
 VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/visual_word_vecs_idx.json"
 VISUAL_WORDS_JSON="${VISUAL_WORD_VECS_DIR}/visual_words.json"
@@ -12,7 +14,7 @@ GLOVE_DIM=300
 VQA_VOCAB_TXT="/home/nfs/tgupta6/Code/pythia/data/vocabulary_vqa.txt"
 
 OUTDIR="/home/nfs/tgupta6/Code/pythia/data"
-VQA_WORD_VECS_NPY="${OUTDIR}/vqa2.multisense_no_syn_cooccur_self_count_dim_100_neg_no_decay_xformed_concat_with_glove_300.npy"
+VQA_WORD_VECS_NPY="${OUTDIR}/vqa2.200_neg_bias_select_concat_with_glove_300.npy"
 
 # Comment out visual_words_json when using glove
 python -m exp.pythia.write_vqa_embeddings \
