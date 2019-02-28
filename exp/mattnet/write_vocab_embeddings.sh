da02ee7c1b4ef4ba680ebe1d54743f7e01d95981
Tanmay Gupta <tanmay2099@gmail.com>
2019-02-27 18:33:57 -0600

    update mattnet sh paths

--- exp/mattnet/write_vocab_embeddings.sh
+++ exp/mattnet/write_vocab_embeddings.sh
@@ -5,13 +5,13 @@ MATTNET_DATA_JSON="/home/nfs/tgupta6/Code/MAttNet/cache/prepro/refcoco+_unc/data
 # VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/glove_6B_300d.h5py"
 # VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/glove_6B_300d_word_to_idx.json"
 
-VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/no_syn_cooccur_self_count_dim_100_neg_no_decay/xformed_concat_with_glove_300"
+VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/effect_of_xforms/dim_200_neg_bias_select/concat_with_glove_300"
 VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/visual_word_vecs.h5py"
 VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/visual_word_vecs_idx.json"
 VISUAL_WORDS_JSON="${VISUAL_WORD_VECS_DIR}/visual_words.json"
 
 
-MATTNET_WORD_VECS_NPY="/home/nfs/tgupta6/Code/MAttNet/cache/prepro/refcoco+_unc/no_syn_cooccur_self_count_dim_100_neg_no_decay_xformed_concat_with_glove_300.npy"
+MATTNET_WORD_VECS_NPY="/home/nfs/tgupta6/Code/MAttNet/cache/prepro/refcoco+_unc/dim_200_neg_bias_select_concat_with_glove_300.npy"
 
 # Comment out visual_words_json when using glove
 python -m exp.mattnet.write_vocab_embeddings \
