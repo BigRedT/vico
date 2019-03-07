f419756d876c4b1ea576dbee70e925ee76707045
Tanmay Gupta <tanmay2099@gmail.com>
2019-03-07 09:23:07 -0600

    path updates

--- exp/captioning/write_vocab_embeddings.sh
+++ exp/captioning/write_vocab_embeddings.sh
@@ -1,5 +1,5 @@
 COCOTALK_JSON="/home/nfs/tgupta6/Code/ImageCaptioning.pytorch/data/cocotalk.json"
-COCOTALK_WORD_VECS_NPY="/home/nfs/tgupta6/Code/ImageCaptioning.pytorch/data/embeddings/dim_50_neg_bias_linear_concat_with_glove_300"
+COCOTALK_WORD_VECS_NPY="/home/nfs/tgupta6/Code/ImageCaptioning.pytorch/data/embeddings/dim_200_neg_bias_linear_concat_with_glove_300"
 USE_GLOVE=False
 
 if [[ "${USE_GLOVE}" = "True" ]]
@@ -17,7 +17,7 @@ then
         --visual_word_vecs_idx_json $VISUAL_WORD_VECS_IDX_JSON
 
 else
-    VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/effect_of_xforms/dim_50_neg_bias_linear/concat_with_glove_300"
+    VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/effect_of_xforms/dim_200_neg_bias_linear/concat_with_glove_300"
     VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/visual_word_vecs.h5py"
     VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/visual_word_vecs_idx.json"
     VISUAL_WORDS_JSON="${VISUAL_WORD_VECS_DIR}/visual_words.json"
--- exp/vsepp/write_vocab_embeddings.sh
+++ exp/vsepp/write_vocab_embeddings.sh
@@ -7,14 +7,14 @@ VOCAB_PKL="/home/nfs/tgupta6/Code/vsepp/vocab/coco_precomp_vocab.pkl"
 
 VISUAL_WORD_VECS_DIR="/home/nfs/tgupta6/Code/visual_word_vecs/symlinks/\
 exp/multi_sense_cooccur/imagenet_genome_gt/effect_of_xforms/\
-dim_50_neg_bias_linear/concat_with_glove_300"
+dim_200_neg_bias_linear/concat_with_glove_300"
 VISUAL_WORD_VECS_H5PY="${VISUAL_WORD_VECS_DIR}/visual_word_vecs.h5py"
 VISUAL_WORD_VECS_IDX_JSON="${VISUAL_WORD_VECS_DIR}/visual_word_vecs_idx.json"
 VISUAL_WORDS_JSON="${VISUAL_WORD_VECS_DIR}/visual_words.json"
 
 
 VSEPP_WORD_VECS_NPY="/home/nfs/tgupta6/Code/vsepp_data/data/coco_precomp/\
-dim_50_neg_bias_linear_concat_with_glove_300.npy"
+dim_200_neg_bias_linear_concat_with_glove_300.npy"
 
 # Comment out visual_words_json when using glove
 python -m exp.vsepp.write_vocab_embeddings \
