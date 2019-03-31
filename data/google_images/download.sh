7aa1147c435036e3260f5636a16c260c0bb4ff76
Tanmay Gupta <tanmay2099@gmail.com>
2019-03-31 17:55:44 +0530

    delete files

--- data/google_images/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-OUTDIR="${PWD}/symlinks/data/google_images"
-VOCAB_JSON="${PWD}/symlinks/data/glove/proc/glove_6B_300d_lemmatized_word_to_idx.json"
-IMAGES_PER_WORD=20
-python -m data.google_images.download_vocab_images \
-    --vocab_json $VOCAB_JSON \
-    --images_per_word  $IMAGES_PER_WORD \
-    --outdir $OUTDIR
