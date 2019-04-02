8da34ebf67738c8dd414c506ff69f3a88823dd85
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-02 15:54:48 +0530

    update explore_merged_cooccur

--- exp/multi_sense_cooccur/explore_merged_cooccur.py
+++ exp/multi_sense_cooccur/explore_merged_cooccur.py
@@ -3,16 +3,35 @@ import pandas as pd
 
 merged_cooccur_csv = os.path.join(
     os.getcwd(),
-    'symlinks/exp/multi_sense_cooccur/imagenet_genome_gt_word/merged_cooccur_self.csv')
+    'symlinks/exp/multi_sense_cooccur/imagenet_genome_gt/merged_cooccur.csv')
 
 print('Reading csv ...')
 df = pd.read_csv(merged_cooccur_csv)
 
-print('Column labels ...')
-labels = df.columns.values.tolist()
-print(labels)
+def show_labels():
+    labels = ['row_id'] + df.columns.values.tolist()
+    print('Column labels: ',labels)
 
-print('Explore ...')
-# df[df.word1=='dog'].sort_values(by='obj_attr')
+def usage():
+    label_str = "- To access column names call 'show_labels()'"
+    example_str = "- Run the following to get co-occurrences for 'leaf' sorted by 'obj_attr': \n\tcooccur('leaf','obj_attr') \n\t\tOR\n\tdf[df.word1=='leaf'].sort_values(by='obj_attr')"
+    ref_str = "- Refer to https://pandas.pydata.org/ for more ways of interacting with dataframe 'df'"
+    usage_str = "- To see usage instructions again call 'usage()'"
+    print('')
+    print('-'*100)
+    print('Usage:')
+    print(label_str)
+    print(example_str)
+    print(ref_str)
+    print(usage_str)
+    print('-'*100)
+    print('')
+
+def cooccur(word1,sort_by):
+    print(df[df.word1==word1].sort_values(by=sort_by))
+    show_labels()
+
+show_labels()
+usage()
 
 import pdb; pdb.set_trace()
