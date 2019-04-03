9ad3e6b015b84eb779af0c7efc4b12ae8e3fe23e
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-03 12:46:26 +0530

    delete runners

--- exp/genome_attributes/run.py
+++ exp/genome_attributes/run.py
@@ -8,10 +8,6 @@ from .dataset import GenomeAttributesDatasetConstants
 from . import create_gt_obj_attr_cooccur
 from . import create_gt_attr_attr_cooccur
 from . import create_gt_context_cooccur
-from . import create_gt_attr_attr_word_cooccur
-from . import create_gt_obj_attr_word_cooccur
-from . import create_gt_context_word_cooccur
-from .vis import vis_pred
 
 
 def exp_create_gt_obj_attr_cooccur():
@@ -52,46 +48,6 @@ def exp_create_gt_context_cooccur():
     data_const = VisualGenomeConstants()
 
     create_gt_context_cooccur.main(exp_const,data_const)
-
-
-def exp_create_gt_attr_attr_word_cooccur():
-    exp_name = 'gt_attr_attr_word_cooccur'
-    out_base_dir = os.path.join(
-        os.getcwd(),
-        'symlinks/exp/genome_attributes')
-    exp_const = ExpConstants(exp_name,out_base_dir)
-    exp_const.batch_size = 32
-    exp_const.num_workers = 5
-
-    data_const = GenomeAttributesDatasetConstants()
-
-    create_gt_attr_attr_word_cooccur.main(exp_const,data_const)
-
-
-def exp_create_gt_obj_attr_word_cooccur():
-    exp_name = 'gt_obj_attr_word_cooccur'
-    out_base_dir = os.path.join(
-        os.getcwd(),
-        'symlinks/exp/genome_attributes')
-    exp_const = ExpConstants(exp_name,out_base_dir)
-    exp_const.batch_size = 32
-    exp_const.num_workers = 5
-
-    data_const = GenomeAttributesDatasetConstants()
-
-    create_gt_obj_attr_word_cooccur.main(exp_const,data_const)
-
-
-def exp_create_gt_context_word_cooccur():
-    exp_name = 'gt_context_word_cooccur'
-    out_base_dir = os.path.join(
-        os.getcwd(),
-        'symlinks/exp/genome_attributes')
-    exp_const = ExpConstants(exp_name,out_base_dir)
-
-    data_const = VisualGenomeConstants()
-
-    create_gt_context_word_cooccur.main(exp_const,data_const)
 
 
 if __name__=='__main__':
--- exp/imagenet/run.py
+++ exp/imagenet/run.py
@@ -5,7 +5,6 @@ from utils.argparse_utils import manage_required_args, str_to_bool
 from utils.constants import Constants, ExpConstants
 from .dataset import ImagenetDatasetConstants
 from . import create_gt_obj_hyp_cooccur
-from . import create_gt_obj_hyp_word_cooccur
 
 
 def exp_create_gt_obj_hyp_cooccur():
@@ -20,20 +19,6 @@ def exp_create_gt_obj_hyp_cooccur():
     data_const = ImagenetDatasetConstants()
 
     create_gt_obj_hyp_cooccur.main(exp_const,data_const)
-
-
-def exp_create_gt_obj_hyp_word_cooccur():
-    exp_name = 'gt_obj_hyp_word_cooccur'
-    out_base_dir = os.path.join(
-        os.getcwd(),
-        'symlinks/exp/imagenet')
-    exp_const = ExpConstants(exp_name,out_base_dir)
-    exp_const.batch_size = 32
-    exp_const.num_workers = 5
-
-    data_const = ImagenetDatasetConstants()
-
-    create_gt_obj_hyp_word_cooccur.main(exp_const,data_const)
 
 
 if __name__=='__main__':
