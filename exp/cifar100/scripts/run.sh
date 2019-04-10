668264ba9b8583d18eda3bf77409390ef27c2b1d
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-10 09:12:13 +0530

    update cifar scripts

--- exp/cifar100/scripts/run.sh
+++ exp/cifar100/scripts/run.sh
@@ -1,9 +1,10 @@
 HELD_CLASSES=$1
 GPU=$2
-bash exp/cifar100/scripts/train.sh $GPU glove 100 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU glove_random 100 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU random 100 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU glove_vico_linear 100 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU glove_vico_linear 200 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU glove_vico_select 200 $HELD_CLASSES
-bash exp/cifar100/scripts/train.sh $GPU glove_random 200 $HELD_CLASSES
+RUN=$3
+SECRUN=$(($RUN+4))
+GLOVE_DIM=100
+# bash exp/cifar100/scripts/train.sh $GPU glove $GLOVE_DIM 0 $HELD_CLASSES $RUN
+# bash exp/cifar100/scripts/train.sh $GPU vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
+# bash exp/cifar100/scripts/train.sh $GPU glove_vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
+bash exp/cifar100/scripts/train.sh $GPU glove_vico_select $GLOVE_DIM 200 $HELD_CLASSES $RUN
+bash exp/cifar100/scripts/train.sh $GPU glove_vico_select $GLOVE_DIM 200 $HELD_CLASSES $SECRUN
--- exp/cifar100/scripts/train.sh
+++ exp/cifar100/scripts/train.sh
@@ -1,9 +1,13 @@
 GPU=$1
-HELD_CLASSES=$4
 EMBED_TYPE=$2
-VICO_DIM=$3
+GLOVE_DIM=$3
+VICO_DIM=$4
+HELD_CLASSES=$5
+RUN=$6
 CUDA_VISIBLE_DEVICES=$GPU python -m exp.cifar100.run \
     --exp exp_train \
     --held_classes $HELD_CLASSES \
     --embed_type $EMBED_TYPE \
-    --vico_dim $VICO_DIM
+    --vico_dim $VICO_DIM \
+    --glove_dim $GLOVE_DIM \
+    --run $RUN
