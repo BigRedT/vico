557c2cbc1d56511233c13cefcad841690d96d96e
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-02 17:53:28 -0500

    install tf and tensorboard

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ conda install -y -c anaconda cudatoolkit=8.0
 conda install -y -c pytorch pytorch=0.3.1 cuda80
 conda install -y -c pytorch torchvision=0.2.0
 conda install -y -c anaconda nltk ujson h5py scipy pandas scikit-learn scikit-image pyyaml
-conda install -y h5py
 conda install -y -c plotly plotly
-conda install -y -c conda-forge tqdm
+conda install -y -c conda-forge tqdm tensorboard
+conda install -y h5py
 pip install tensorboard_logger
