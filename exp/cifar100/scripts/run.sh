176396c312a39dd87375cdd32b7a23d87525772e
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-02 13:30:26 -0500

    update cifar run.sh

--- exp/cifar100/scripts/run.sh
+++ exp/cifar100/scripts/run.sh
@@ -1,10 +1,8 @@
 HELD_CLASSES=$1
 GPU=$2
 RUN=$3
-SECRUN=$(($RUN+4))
 GLOVE_DIM=100
-# bash exp/cifar100/scripts/train.sh $GPU glove $GLOVE_DIM 0 $HELD_CLASSES $RUN
-# bash exp/cifar100/scripts/train.sh $GPU vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
-# bash exp/cifar100/scripts/train.sh $GPU glove_vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
+bash exp/cifar100/scripts/train.sh $GPU glove $GLOVE_DIM 0 $HELD_CLASSES $RUN
+bash exp/cifar100/scripts/train.sh $GPU vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
+bash exp/cifar100/scripts/train.sh $GPU glove_vico_linear $GLOVE_DIM 100 $HELD_CLASSES $RUN
 bash exp/cifar100/scripts/train.sh $GPU glove_vico_select $GLOVE_DIM 200 $HELD_CLASSES $RUN
-bash exp/cifar100/scripts/train.sh $GPU glove_vico_select $GLOVE_DIM 200 $HELD_CLASSES $SECRUN
