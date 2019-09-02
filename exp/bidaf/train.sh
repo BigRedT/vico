23233530d65c77d3b3c4b1eba2c75c09bccd9957
Tanmay Gupta <tanmay2099@gmail.com>
2019-09-01 21:16:38 -0500

    delete bidaf

--- exp/bidaf/train.sh
+++
@@ -1,6 +0,0 @@
-DIM=100
-JSONNET="${PWD}/exp/bidaf/bidaf_${DIM}.jsonnet"
-OUTDIR="${PWD}/symlinks/exp/bidaf/dim_${DIM}_neg_bias_linear_concat_with_glove_100/run_0"
-echo $JSONNET
-echo $OUTDIR
-allennlp train $JSONNET -s $OUTDIR
